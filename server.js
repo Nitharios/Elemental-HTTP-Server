3c7fd789c62b4e0f1df80e2d2f26a5de66b44024
Nathan N <Nitharios@users.noreply.github.com>
2017-10-05 11:47:15 -1000

    Cleaned up code

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer((req, res) => {
 
   if (method === 'GET') {
     uri = req.url;      
-    handlers.getFiles(res, uri);
+    handlers.getRequest(res, uri);
   
   } else if (method === 'POST') {
     req.on('data', (data) => {
@@ -32,41 +32,4 @@ const server = http.createServer((req, res) => {
 
 /* FUNCTIONS */
 
-// will take in the route requested and output the correct file
-// function getFiles(response, route) {
-//   // handles content type
-//   if (route === '/css/styles.css') type = 'text/css';
-//   else type = 'text/html';
-
-//   // handles navigation to main page
-//   if (route === '/') route = `/index.html`;
-
-//   // sets the correct route to read from
-//   source = path.join('public', route);
-
-//   fs.readFile(source, format, (err, data) => {
-//     // returns 404 page is there is a problem with reading the file
-//     if (err) getFiles(response, errorPath);
-    
-//     // if there is data, include a 200 and Content-Type header in the reponse header
-//     else if (data) {response.writeHead(200, {'Content-Type' : type});
-//       // then write the data to the response
-//       response.write(data, (err) => {
-//         // if an error is thrown , this would be a server side error???
-//         if (err) throw err;
-//       });
-        
-//       response.end();
-    
-//     }
-//   });
-// }
-
 // function processPostRequest(request, route )
-
-function postFiles(response, route) {
-  console.log('post');
-
-  // fs.writeFile...
-
-}
