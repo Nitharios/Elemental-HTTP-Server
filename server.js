30aef9ff150aadfe9c5f582c98b6581f0cd42775
Nathan N <Nitharios@users.noreply.github.com>
2017-10-04 17:00:14 -1000

    Building up server

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8888;
 
+const files = {
+  'index.html' : 'index.html',
+  'helium.html' : 'helium.html',
+  'hydrogen.html' : 'hydrogen.html',
+  'styles.css' : 'css/styles.css'
+};
+
 const server = http.createServer((req, res) => {
   console.log('req', req);
   console.log('res', res);
@@ -18,3 +25,14 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+/* FUNCTIONS */
+
+// will take in the path requested and output the correct file
+function readDirectoryFiles(response, path) {
+  fs.readFile(path, (data, err) => {
+    if (err) throw err;
+
+    else if (files.hasOwnProperty(path)) response.write(data);
+  });
+}
