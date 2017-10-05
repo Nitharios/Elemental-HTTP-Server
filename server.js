6ce9a81d1cbbe7eb46a9140be5c31db0ad702410
Nathan N <Nitharios@users.noreply.github.com>
2017-10-04 20:23:01 -1000

    Cleaning up code

--- server.js
+++ server.js
@@ -10,29 +10,27 @@ let format  = 'UTF-8';
 let method, source, type, uri;
 
 const server = http.createServer((req, res) => {
-  // console.log('req', req);
-  // console.log('res', res);
   console.log('Request Initiated');
 
   method = req.method;
 
   if (method === 'GET') {
     uri = req.url;
-
     readDirectoryFiles(res, uri);
-
   }
+
 }).listen(PORT);
 
 /* FUNCTIONS */
 
 // will take in the path requested and output the correct file
 function readDirectoryFiles(response, path) {
-  
-  if (path === '/') path = `/index.html`;
-
+  // handles content type
   if (path === '/css/styles.css') type = 'text/css';
   else type = 'text/html';
+
+  // handles navigation
+  if (path === '/') path = `/index.html`;
 
   source = `./public${path}`;
 
@@ -44,9 +42,12 @@ function readDirectoryFiles(response, path) {
     
     if (data) {response.writeHead(200, {'Content-Type' : `${type}`});
       response.write(data, (err) => {
+
         if (err) throw err;
       });
-      
-      response.end();}
+        
+      response.end();
+    
+    }
   });
 }
