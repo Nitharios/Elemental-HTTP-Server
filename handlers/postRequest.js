7bc6a8db26fff113f8d5f6607c67f6613e3baba6
Nathan N <Nitharios@users.noreply.github.com>
2017-10-05 12:35:57 -1000

    Added functionality

--- handlers/postRequest.js
+++ handlers/postRequest.js
@@ -5,8 +5,11 @@ const fs = require('fs');
 const path = require('path');
 const qs = require('querystring');
 
-/* FUNCTIONS */
+let formData, parsedData, newPath;
+// __dirname
+let publicDir = '../public/';
 
+/* FUNCTIONS */
 
 function postRequest(request, response, data) {
   processPostRequest(request, response, data);  
@@ -14,8 +17,19 @@ function postRequest(request, response, data) {
 }
 
 function processPostRequest(request, response, data) {
+  // handles the body of data attached to POST request header
+  // ASYNC so will continue to next function while still processing
   request.on('data', (data) => {
-    console.log('data', data.toString());
+    // passes data to a variable as a string (otherwise it is a buffer)
+    formData = data.toString().toLowerCase();
+    // parses data into a collection of key and value pairs
+    parsedData = qs.parse(formData);
+    console.log(parsedData);
+
+    // fs.writeFile();
+
+
+    response.end();
   });
 }
 
