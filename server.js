1ff5b55aefdcd99f764659658d0e09b5dcae3fb6
Nathan N <Nitharios@users.noreply.github.com>
2017-10-05 11:54:30 -1000

    Cleaned up code

--- server.js
+++ server.js
@@ -3,29 +3,31 @@ const sanity = "You're not crazy!";
 console.log(sanity);
 
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const handlers = require('./handlers/handlers');
-
 const PORT = process.env.PORT || 8888;
 
-let errorPath = '/error.html';
-let format  = 'UTF-8';
-let method, source, type, uri;
+let method, uri;
 
 const server = http.createServer((req, res) => {
   console.log('Request Initiated');
 
   method = req.method;
 
-  if (method === 'GET') {
-    uri = req.url;      
-    handlers.getRequest(res, uri);
-  
-  } else if (method === 'POST') {
-    req.on('data', (data) => {
-      console.log('data', data.toString());
-    });
+  switch (method) {
+    case 'HEAD':
+      handlers.headRequest(res, uri);
+      break;
+    case 'GET':
+      uri = req.url;
+      handlers.getRequest(res, uri);
+      break;
+    case 'POST':
+      req.on('data', (data) => {
+        console.log('data', data.toString());
+      });
+      break;
+    default:
+      handlers.getRequest(res, 'error');
   }
 
 }).listen(PORT);
