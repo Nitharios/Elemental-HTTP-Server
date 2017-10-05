9a4cfc7a5745c833f198f418743e504e96f010da
Nathan N <Nitharios@users.noreply.github.com>
2017-10-04 16:05:38 -1000

    Setup basic server

---
+++ server.js
@@ -0,0 +1,20 @@
+/* jshint esversion:6 */
+const sanity = "You're not crazy!";
+console.log(sanity);
+
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 8888;
+
+const server = http.createServer(req, res, () => {
+  console.log('req', req);
+  console.log('res', res);
+
+  res.write("Welcome user!");
+  res.end();
+
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
