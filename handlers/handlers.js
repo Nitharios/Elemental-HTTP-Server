5d3934fbc38ef2ce286e24a60362a8e5c71049f9
Nathan N <Nitharios@users.noreply.github.com>
2017-10-05 11:47:41 -1000

    Cleaned up file organization

---
+++ handlers/handlers.js
@@ -0,0 +1,5 @@
+/* jshint esversion:6 */
+module.exports = {
+  getRequest : require('./getRequest'),
+  postRequest : require('./postRequest')
+};
---
+++ handlers/postRequest.js
@@ -0,0 +1,17 @@
+/* jshint esversion:6 */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+/* FUNCTIONS */
+
+function processPostRequest(request, response, data) {
+
+}
+
+function postRequest(request, response, data) {
+  // fs.writeFile()
+}
+
+module.exports = postRequest;
