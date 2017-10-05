0d6c39a8885f698611252029d57054a198c5bd25
Nathan N <Nitharios@users.noreply.github.com>
2017-10-04 19:56:36 -1000

    Able to load website and navigate using links

--- server.js
+++ server.js
@@ -6,25 +6,17 @@ const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8888;
 
-const files = {
-  'index.html' : 'index.html',
-  'helium.html' : 'helium.html',
-  'hydrogen.html' : 'hydrogen.html',
-  'styles.css' : 'css/styles.css'
-};
-
 let format  = 'UTF-8';
-let method, source, uri;
+let method, source, type, uri;
 
 const server = http.createServer((req, res) => {
   // console.log('req', req);
   // console.log('res', res);
-  console.log('Someone connected!');
+  console.log('Connection established!');
 
   method = req.method;
 
   if (method === 'GET') {
-    console.log(method);
     uri = req.url;
 
     readDirectoryFiles(res, uri);
@@ -37,21 +29,24 @@ const server = http.createServer((req, res) => {
 // will take in the path requested and output the correct file
 function readDirectoryFiles(response, path) {
   
-  if (path === '/') path = `index.html`;
-  
-  if (files.hasOwnProperty(path)) {
-    if (path === 'styles.css') path = `css/${path}`;
-    source = `./public/${path}`;
-    console.log('source', source);
-  }
+  if (path === '/') path = `/index.html`;
+
+  if (path === '/css/styles.css') type = 'text/css';
+  else type = 'text/html';
+
+  source = `./public${path}`;
 
   fs.readFile(source, format, (err, data) => {
-    if (err) throw err;
-
-  response.write(data.toString(), (err) => {
-      if (err) throw err;
-
-      response.end();
-    });
+    if (err) {
+      // gets rid of the favicon error and shoves it to console
+      console.log(err);
+    }
+    
+    if (data) {response.writeHead(200, {'Content-Type' : `${type}`});
+      response.write(data, (err) => {
+        if (err) throw err;
+      });
+      
+      response.end();}
   });
 }
