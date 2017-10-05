04ee846967d93eb7c189a8bfe9e3c95257e0ff1a
Nathan N <Nitharios@users.noreply.github.com>
2017-10-05 11:58:04 -1000

    Added to postRequest functions

--- handlers/postRequest.js
+++ handlers/postRequest.js
@@ -6,12 +6,16 @@ const path = require('path');
 
 /* FUNCTIONS */
 
-function processPostRequest(request, response, data) {
-
-}
 
 function postRequest(request, response, data) {
+  processPostRequest(request, response, data);  
   // fs.writeFile()
+}
+
+function processPostRequest(request, response, data) {
+  request.on('data', (data) => {
+    console.log('data', data.toString());
+  });
 }
 
 module.exports = postRequest;
